bebfc9e41e5a6032950494eedb25144d7471f8d4
Crolsic63 <165874272+Crolsic63@users.noreply.github.com>
2025-09-18 20:30:22 +0400

    Create Taimer.js

---
+++ Taimer.js
@@ -0,0 +1,20 @@
+// let Id = setInterval(my_func, 1000);
+
+// let count = 0;
+// function my_func() {
+//     count++;
+//     console.log(count);
+//     if (count >= 5) {
+//         clearInterval(Id);
+//     }
+// }
+
+// setInterval(function () {
+//     console.log('Привет!!!');
+//     count++;
+//     console.log(count);
+// }, 2000);
+
+setTimeout(function () {
+    console.log("Привет!!!");
+}, 3000);
